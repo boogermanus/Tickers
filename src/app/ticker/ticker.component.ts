6fdc60babaf94a742169550592493f0e67731680
Jordan Woodruff <jordan@neko.local>
2017-07-20 18:43:50 -0500

    Count Up/Down

--- src/app/ticker/ticker.component.ts
+++ src/app/ticker/ticker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, EventEmitter } from '@angular/core';
 import {Subscription} from 'rxjs';
 import { Observable } from 'rxjs/Rx';
 
@@ -8,18 +8,40 @@ import { Observable } from 'rxjs/Rx';
   styleUrls: ['./ticker.component.css']
 })
 export class TickerComponent implements OnInit, OnDestroy {
-
+  @Input() mode: string;
+  @Input() from: number;
+  emitter: EventEmitter<boolean>;
   constructor() { }
   ticks:number = 0;
   timerSubscription: Subscription;
+  done:boolean = false;
 
   ngOnInit() {
+
+    if(this.mode == "D") {
+      this.ticks = this.from;
+    }
     let timer = Observable.timer(2000,1000);
-    this.timerSubscription = timer.subscribe(t => this.ticks = t)
+    this.timerSubscription = timer.subscribe((t) => {
+      this.updateTicks(t);
+    })
   }
 
   ngOnDestroy() {
     this.timerSubscription.unsubscribe();
+  }
+
+  updateTicks(pNumber:number) {
+    if(this.mode == "U") {
+      this.ticks = pNumber
+    }
+    else {
+      this.ticks--;
+      if(this.ticks == 0) {
+        this.timerSubscription.unsubscribe();
+        this.done = true;
+      }
+    }
   }
 
 }
