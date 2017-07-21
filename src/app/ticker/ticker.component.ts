ea5e3e41775ab9c952dfc90dda3e6b16381311f3
Jordan Woodruff <jordan@neko.local>
2017-07-20 20:02:49 -0500

    Count Up/Down start/w parameter

--- src/app/ticker/ticker.component.ts
+++ src/app/ticker/ticker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, Input, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import {Subscription} from 'rxjs';
 import { Observable } from 'rxjs/Rx';
 
@@ -8,40 +8,52 @@ import { Observable } from 'rxjs/Rx';
   styleUrls: ['./ticker.component.css']
 })
 export class TickerComponent implements OnInit, OnDestroy {
-  @Input() mode: string;
-  @Input() from: number;
-  emitter: EventEmitter<boolean>;
+  mode: string = "D"; //count up
+  from: number;
   constructor() { }
   ticks:number = 0;
   timerSubscription: Subscription;
   done:boolean = false;
+  timer:Observable<number>;
 
   ngOnInit() {
 
-    if(this.mode == "D") {
-      this.ticks = this.from;
-    }
-    let timer = Observable.timer(2000,1000);
-    this.timerSubscription = timer.subscribe((t) => {
-      this.updateTicks(t);
-    })
   }
 
   ngOnDestroy() {
-    this.timerSubscription.unsubscribe();
+    if(!this.timerSubscription.closed)
+      this.timerSubscription.unsubscribe();
   }
 
   updateTicks(pNumber:number) {
+    let finished = false;
     if(this.mode == "U") {
       this.ticks = pNumber
+      
+      if(this.ticks == this.from)
+        finished = true;
     }
     else {
       this.ticks--;
       if(this.ticks == 0) {
-        this.timerSubscription.unsubscribe();
-        this.done = true;
+        finished = true;
       }
+    }
+
+    if(finished) {
+      this.timerSubscription.unsubscribe();
+      this.done = true;
     }
   }
 
+  start() {
+    this.done = false;
+    if(this.mode == "D")
+      this.ticks = this.from + 1;
+
+    this.timer = Observable.timer(0, 1000);
+    this.timerSubscription = this.timer.subscribe((t) => {
+      this.updateTicks(t);
+    })
+  }
 }
