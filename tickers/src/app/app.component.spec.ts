091f1508475c474cc20725a8fc7340aabe03e8e6
Jordan Woodruff <boogermanus@gmail.com>
2025-01-16 17:31:10 -0600

    tests working after delete

--- tickers/src/app/app.component.spec.ts
+++ tickers/src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tickers');
+    expect(compiled.querySelector('p')?.textContent).toContain('tickers');
   });
 });
