6b96055ac7f43f0626990a48217a83747bc93b25
Jordan Woodruff <boogermanus@gmail.com>
2025-01-18 12:44:52 -0600

    add card

--- tickers/src/app/components/ticker/ticker.component.ts
+++ tickers/src/app/components/ticker/ticker.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-ticker',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule
+  ],
   templateUrl: './ticker.component.html',
   styleUrl: './ticker.component.scss'
 })
