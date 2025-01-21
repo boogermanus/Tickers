3b8874b5d4bcac0cee71e7b265fa592ce83128d5
Jordan Woodruff <boogermanus@gmail.com>
2025-01-21 11:08:25 -0600

    service work and tests

--- tickers/src/app/services/ticker.service.spec.ts
+++ tickers/src/app/services/ticker.service.spec.ts
@@ -13,4 +13,48 @@ describe('TickerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have method start()', () => {
+    expect(service.start).toBeDefined();
+  });
+
+  it('should set running to true when start() is called', () => {
+    service.start();
+    expect(service.Running()).toBeTrue();
+  });
+
+  it('should set done to false when start() is called', () => {
+    service.start();
+    expect(service.Done()).toBeFalse();
+  });
+
+  it('should have method config()', () => {
+    expect(service.config).toBeDefined();
+  });
+
+  it('should set Ticks to 0 when config() is called with COUNT_UP', () => {
+    service.config(1, TickerService.COUNT_UP);
+    expect(service.Ticks()).toEqual(0);
+  });
+
+  it('should set Ticks to 1 when config() is called with COUNT_DOWN', () => {
+    service.config(1, TickerService.COUNT_DOWN);
+    expect(service.Ticks()).toEqual(1);
+  });
+
+  it('should have method stop()', () => {
+    expect(service.stop).toBeDefined();
+  });
+
+  it('should set running to false when stop is called', () => {
+    service.start();
+    service.stop();
+    expect(service.Running()).toBeFalse();
+  })
+
+  it('should call unsubscribe() when stop() is called', () => {
+    let spy = spyOn<any>(service, 'unsubscribe');
+    service.stop();
+    expect(spy).toHaveBeenCalled();
+  })
 });
--- tickers/src/app/services/ticker.service.ts
+++ tickers/src/app/services/ticker.service.ts
@@ -15,13 +15,57 @@ export class TickerService {
   }
 
   private timerSubscription: Subscription = new Subscription();
-  private done = false;
   private timer!: Observable<number>;
+
+  private done = signal(false);
+  public get Done(): Signal<boolean> {
+    return this.done.asReadonly();
+  }
   
   private running = signal(false);
   public get Running(): Signal<boolean> {
     return this.running.asReadonly();
   }
-  
+
+  private minutes = signal('00');
+  public get Minutes(): Signal<string> {
+    return this.minutes.asReadonly();
+  }
+
+  private seconds = signal('00');
+  public get Seconds(): Signal<string> {
+    return this.seconds.asReadonly();
+  }
+
+  private mode!: string;
+  private target!: number;
+
   constructor() { }
+
+  public start(): void {
+    this.running.set(true);
+    this.done.set(false);
+  }
+
+  public config(ticks: number, mode: string): void {
+
+    this.ticks.set(0);
+    this.target = ticks;
+
+    if(mode === TickerService.COUNT_DOWN) {
+      this.ticks.set(ticks);
+      this.target = 0;
+    }
+  }
+
+  public stop(): void {
+    this.running.set(false);
+    this.unsubscribe();
+  }
+
+  private unsubscribe(): void {
+    if(!this.timerSubscription.closed) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
 }
