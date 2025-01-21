c89069ee8f126b3e56c85faa86c0d47583647be1
Jordan Woodruff <boogermanus@gmail.com>
2025-01-21 11:54:39 -0600

    fixing test

--- tickers/src/app/components/ticker/ticker.component.ts
+++ tickers/src/app/components/ticker/ticker.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-ticker',
   standalone: true,
   imports: [
     MatCardModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './ticker.component.html',
   styleUrl: './ticker.component.scss'
