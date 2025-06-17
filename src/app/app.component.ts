438e6cdb2e62ebc163866e392e5d203257f10a5b
Jordan Woodruff <boogermanus@gmail.com>
2025-06-16 19:13:14 -0500

    Angular19 (#57)
    * angular19
    
    * update material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { TickerComponent } from "./components/ticker/ticker.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    TickerComponent
-],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [
+        TickerComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'tickers';
